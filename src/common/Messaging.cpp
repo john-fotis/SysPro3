0c8afcd18bb4b93290f781eb66fd7d760ec71b16
John Fotis <giannisfotis@gmail.com>
2021-05-31 19:35:50 +0300

    Replaced wirte/read with send/recv

--- src/common/Messaging.cpp
+++ src/common/Messaging.cpp
@@ -19,37 +19,37 @@ void sendPackets(int fdWrite, const char *data, unsigned int msgSize, unsigned i
     for (unsigned int i = 0; i < padding; i++)
         message[(numOfPackets * buffSize) - padding + i] = '\0';
 
-    // // Send the number of packets
-    // fifoWrite(fdWrite, &numOfPackets, sizeof(int));
-    // // Send the padding of the last packet
-    // fifoWrite(fdWrite, &padding, sizeof(int));
+    // Send the number of packets
+    writeToSocket(fdWrite, &numOfPackets, sizeof(int));
+    // Send the padding of the last packet
+    writeToSocket(fdWrite, &padding, sizeof(int));
 
-    // char packet[buffSize + 1] = {'\0'};
-    // // Send the data in packets
-    // for (unsigned int i = 0; i < numOfPackets; i++) {
-    //     memcpy(packet, message + (i * buffSize), buffSize);
-    //     fifoWrite(fdWrite, packet, buffSize);
-    // }
+    char packet[buffSize + 1] = {'\0'};
+    // Send the data in packets
+    for (unsigned int i = 0; i < numOfPackets; i++) {
+        memcpy(packet, message + (i * buffSize), buffSize);
+        writeToSocket(fdWrite, packet, buffSize);
+    }
 }
 
 // Returns char pointer to the read message
 char *receivePackets(int fdRead, unsigned int buffSize) {
     unsigned int numOfPackets = 0, padding = 0;
     
-    // // Read the number of packets
-    // fifoRead(fdRead, &numOfPackets, sizeof(int));
-    // // Read the padding of the last packet
-    // fifoRead(fdRead, &padding, sizeof(int));
+    // Read the number of packets
+    readFromSocket(fdRead, &numOfPackets, sizeof(int));
+    // Read the padding of the last packet
+    readFromSocket(fdRead, &padding, sizeof(int));
 
     // The initial buffer will hold the message along with the padding
     char message[numOfPackets * buffSize + 1] = {0};
 
     char packet[buffSize + 1] = {'\0'};
-    // // Read the actual data
-    // for (unsigned int i = 0; i < numOfPackets; i++) {
-    //     fifoRead(fdRead, packet, buffSize);
-    //     memcpy(message + (i * buffSize), packet, buffSize);
-    // }
+    // Read the actual data
+    for (unsigned int i = 0; i < numOfPackets; i++) {
+        readFromSocket(fdRead, packet, buffSize);
+        memcpy(message + (i * buffSize), packet, buffSize);
+    }
     
     // Now we cut off the padding and get the original message
     char *data = new char[(numOfPackets * buffSize) - padding];
