54c88d7602a8f45f3d07d43769346c822bcfceb0
John Fotis <giannisfotis@gmail.com>
2021-05-31 19:34:47 +0300

    Implemented socket create, bind, listen, accept, close, write and read

--- include/SocketLibrary.hpp
+++ include/SocketLibrary.hpp
@@ -9,7 +9,16 @@
 
 #include <iostream>
 
-int createSocket();
+#define MIN_PORT 1024
+#define MAX_PORT 65535
+#define MAX_CONNECTIONS 5
 
+int createSocket();
+void bindSocket(int &sock, const sockaddr *sockPtr, socklen_t len);
+void listenConnections(int sockfd, int maxConn);
+int acceptConnection(int sockfd, sockaddr *addr, socklen_t *len);
+void closeSocket(int sock);
+void writeToSocket(int sockfd, void *message, size_t bufferSize);
+void readFromSocket(int sockfd, void *message, size_t bufferSize);
 
 #endif
--- src/common/SocketLibrary.cpp
+++ src/common/SocketLibrary.cpp
@@ -9,6 +9,38 @@
 
 int createSocket() {
     int sock = 0;
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) die("createSocket", 11);
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) die("socket/create", 11);
     return sock;
+}
+
+void bindSocket(int &sock, const sockaddr *sockPtr, socklen_t len) {
+    if (bind(sock, sockPtr, len) < 0) die("socket/bind", 12);
+}
+
+void listenConnections(int sockfd, int maxConn) {
+    if(listen(sockfd, maxConn) < 0) die("socket/listen", 13);
+}
+
+int acceptConnection(int sockfd, sockaddr *addr, socklen_t *len) {
+    int newsock = 0;
+    if ((newsock = accept(sockfd, addr, len)) < 0) die("socket/accept", 14);
+    return newsock;
+}
+
+void closeSocket(int sock) {
+    if (close(sock) < 0) die("socket/close", 15);
+}
+
+void writeToSocket(int sockfd, void *message, size_t bufferSize) {
+    if (send(sockfd, message, bufferSize, 0) < 0) {
+        perror("socket/write");
+        exit(16);
+    }
+}
+
+void readFromSocket(int sockfd, void *message, size_t bufferSize) {
+    if (recv(sockfd, message, bufferSize, 0) < 0) {
+        perror("socket/read");
+        exit(17);
+    }
 }
